ac0f1404948ab3f64f1aa992d71810f826d1dcfb
Jane Nwachukwu <jane46818@gmail.com>
2025-12-31 02:20:35 +0100

    Add SearchHistory model to store search data

--- weather-forecast-app/weather/models.py
+++ weather-forecast-app/weather/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class SearchHistory(models.Model):
+    """Model to store search history"""
+    city_name = models.CharField(max_length=100)
+    temperature = models.FloatField(null=True, blank=True)
+    weather_description = models.CharField(max_length=100, null=True, blank=True)
+    humidity = models.IntegerField(null=True, blank=True)
+    wind_speed = models.FloatField(null=True, blank=True)
+    country = models.CharField(max_length=10, null=True, blank=True)
+    date_searched = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.city_name} - {self.date_searched.strftime('%Y-%m-%d %H:%M')}"
+
+    class Meta:
+        ordering = ['-date_searched']
+        verbose_name_plural = 'Search Histories'
