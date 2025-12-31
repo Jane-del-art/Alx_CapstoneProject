fd9e31521c699767d39bec7d55dc6cd5005e92ff
Jane Nwachukwu <jane46818@gmail.com>
2025-12-31 02:19:49 +0100

    Register SearchHistory model in Django admin

--- weather-forecast-app/weather/admin.py
+++ weather-forecast-app/weather/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import SearchHistory
 
-# Register your models here.
+@admin.register(SearchHistory)
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ('city_name', 'temperature', 'weather_description', 'country', 'date_searched')
+    list_filter = ('country', 'date_searched')
+    search_fields = ('city_name', 'country')
+    readonly_fields = ('date_searched',)
+    ordering = ('-date_searched',)
